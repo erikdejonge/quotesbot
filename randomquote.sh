7088b3ca1ca7481012156567cebe0753c21c2c90
Erik de Jonge <erik@a8.nl>
2019-12-17 12:18:28 +0100

    -

--- randomquote.sh
+++ randomquote.sh
@@ -1,1 +1,1 @@
-shuf -n 1 allquotes2.tsv | puf '[print("\033[90m"+x.split("\t")[0].strip()+":\033[0m\n\n\033[33m"+x.split("\t")[2]+"\n\n\033[92m- "+x.split("\t")[1])  for x in lines]'
+shuf -n 1 /Users/rabshakeh/workspace/forks/quotesbot/allquotes2.tsv | puf '[print("\033[90m"+x.split("\t")[0].strip()+":\033[0m\n\n\033[33m"+x.split("\t")[2]+"\n\n\033[92m- "+x.split("\t")[1].strip())  for x in lines]'| puf "'\n'.join(lines).strip()"
